57e58889318e87f701ce1581353cad4a85bcd47c
LaurinGob <113621416+LaurinGob@users.noreply.github.com>
2025-10-16 14:26:40 +0200

    Added UnitTest Project to Solution

---
+++ MeetUpTogether.UnitTests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace MeetUpTogether.UnitTests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
