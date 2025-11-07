d65e65927b552f6f578e1f30a32060bb7058ca2e
LaurinGob <113621416+LaurinGob@users.noreply.github.com>
2025-11-07 22:59:14 +0100

    Added Models

---
+++ MeetUpTogether.UI/Models/Note.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MeetUpTogether.UI.Models
+{
+    public class Note
+    {
+        public string Content { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+    }
+}
