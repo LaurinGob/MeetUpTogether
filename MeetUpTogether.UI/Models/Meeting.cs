ce2787b12911ec4baff26aef27df2cf4be81e3ab
LaurinGob <113621416+LaurinGob@users.noreply.github.com>
2025-11-07 22:59:55 +0100

    Update Meeting.cs

--- MeetUpTogether.UI/Models/Meeting.cs
+++ MeetUpTogether.UI/Models/Meeting.cs
@@ -1,12 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace MeetUpTogether.UI.Models
 {
-    class Meeting
+    public class Meeting
     {
+        public string Title { get; set; }
+        public DateTime From { get; set; }
+        public DateTime To { get; set; }
+        public string Agenda { get; set; }
+        public ObservableCollection<Note> Notes { get; set; } = new();
     }
 }
